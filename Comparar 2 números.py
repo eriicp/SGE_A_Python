2b25c8385b323e4cb8c0dc9537ad9d028a61faa5
eriicp <eric.parada.r@gmail.com>
2024-11-12 17:13:09 +0100

    python

---
+++ Comparar 2 números.py
@@ -0,0 +1,8 @@
+A = 1
+B = 2
+if A > B:
+    print(f" A {A} es mes gran que B {B}")
+elif A < B:
+    print(f" A {A} es mes petit que B {B}")
+elif A == B:
+    print (f"{A} A es igual que B {B}")
---
+++ Dia de la setmana.py
@@ -0,0 +1,15 @@
+dia_setmana = "dilluns"
+if dia_setmana == "dilluns":
+    print("Avui és dilluns")
+elif dia_setmana == "dilluns":
+    print("Avui és dilluns")
+elif dia_setmana == "dilluns":
+    print("Avui és dilluns")
+elif dia_setmana == "dilluns":
+    print("Avui és dilluns")
+elif dia_setmana == "dilluns":
+    print("Avui és dilluns")
+elif dia_setmana == "dilluns":
+    print("Avui és dilluns")
+elif dia_setmana == "dilluns":
+    print("Avui és dilluns")
